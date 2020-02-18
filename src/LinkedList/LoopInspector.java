8ae4160fdece19f43968bab335654a18e3811540
joan parellada <joan.parellada789@gmail.com>
2020-02-18 18:03:40 +0100

    last changes at linkedlist

--- src/LinkedList/LoopInspector.java
+++ src/LinkedList/LoopInspector.java
@@ -1,4 +1,23 @@
 package LinkedList;
 
 public class LoopInspector {
+    public int loopSize(Node node) {
+        Node slow = node;
+        Node fast = node.next;
+        while (true){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                Node mark = slow;
+                slow = slow.next;
+                int counter = 2;
+                while (mark != slow){
+                    slow = slow.next;
+                    counter += 1;
+                }
+                return counter;
+            }
+        }
+    }
+
 }
